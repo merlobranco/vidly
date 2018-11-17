088f8ffe4fc5435d902766cb1050456fc7723c20
Brais Cidras <braisomary@gmail.com>
2018-11-17 21:47:48 +0200

    Working with Data

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -20,6 +20,9 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        // Required for dealing with Customers table
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
