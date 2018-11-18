a0bc9ba95e7d5ccd0228b7fc5617a00181e41257
Brais Cidras <braisomary@gmail.com>
2018-11-18 13:17:06 +0200

    Building Forms

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -20,11 +20,9 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        // Required for dealing with Customers table
         public DbSet<Customer> Customers { get; set; }
-        
-        // Required for dealing with Movies table
         public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MemberShipTypes { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
