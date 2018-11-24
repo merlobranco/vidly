efe15979a762c6bcf12537cb674c91f6719d63ff
Brais Cidras <braisomary@gmail.com>
2018-11-24 12:42:34 +0200

    Performance Optimization

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -24,7 +24,7 @@ namespace Vidly.Controllers
         public ViewResult Index()
         {
             //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-            //return View(customers);
+            //return View(customers);  
             return View();
         }
 
